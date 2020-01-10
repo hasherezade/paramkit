75b5d1f9809c877829562d2cfd1128cb53feac3c
hasherezade <hasherezade@gmail.com>
2020-01-11 00:19:13 +0100

    [FEATURE] Print info about invalid param

--- paramkit/params.cpp
+++ paramkit/params.cpp
@@ -1,7 +1,11 @@
 #include "params.h"
 
+#define WARNING_COLOR 0x0c
 #define HILIGHTED_COLOR 0x0f
+
 #define HEADER_COLOR 14
+#define SEPARATOR_COLOR 6
+
 #define PARAM_HELP "?"
 
 using namespace paramkit;
@@ -21,6 +25,12 @@ namespace paramkit {
     void print_param_in_color(int color, const std::string &text)
     {
         print_in_color(color, PARAM_SWITCH1 + text);
+    }
+
+    void print_unknown_param(const char *param)
+    {
+        print_in_color(WARNING_COLOR, "Invalid parameter: ");
+        std::cout << param << "\n";
     }
 
     bool is_param(const char *str)
@@ -99,6 +109,7 @@ bool Params::parse(int argc, char* argv[])
         if (!is_param(argv[i])) {
             continue;
         }
+        bool found = false;
         const char *param_str = &argv[i][1];
 
         std::map<std::string, Param*>::iterator itr;
@@ -112,16 +123,25 @@ bool Params::parse(int argc, char* argv[])
             if (!strcmp(param_str, param->argStr.c_str())) {
                 if ((i + 1) < argc && !(is_param(argv[i + 1]))) {
                     param->parse(argv[i + 1]);
-                    count++;
+                    found = true;
 #ifdef _DEBUG
                     std::cout << argv[i] << " : " << argv[i + 1] << "\n";
 #endif
+                    break;
                 }
                 else if (!param->requiredParam) {
                     param->parse(nullptr);
-                    count++;
+                    found = true;
+                    break;
                 }
             }
+        }
+        if (found) {
+            count++;
+        }
+        else {
+            print_unknown_param(argv[i]);
+            return false;
         }
     }
     return (count > 0) ? true : false;
