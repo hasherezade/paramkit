9c09a7d8d8ab7fc795b730d7e5b9cdd9120f820b
hasherezade <hasherezade@gmail.com>
2021-08-02 17:07:26 +0200

    [FEATURE][demo] Cleaned up. Added a new example

--- demo/main.cpp
+++ demo/main.cpp
@@ -4,8 +4,11 @@
 #define PARAM_MY_DEC "pdec"
 #define PARAM_MY_HEX "phex"
 
-#define PARAM_MY_STRING "pstr"
+#define PARAM_MY_ASTRING "pastr"
+#define PARAM_MY_WSTRING "pwstr"
+
 #define PARAM_MY_BOOL "pbool"
+#define MAX_BUF 50
 
 using namespace paramkit;
 
@@ -13,7 +16,8 @@ typedef struct {
     DWORD myDec;
     DWORD myHex;
     bool myBool;
-    char myBuf[50];
+    char myABuf[MAX_BUF];
+    wchar_t myWBuf[MAX_BUF];
 } t_params_struct;
 
 void print_params(t_params_struct &p)
@@ -21,7 +25,8 @@ void print_params(t_params_struct &p)
     std::cout << "myDec:  " << std::dec << p.myDec << "\n";
     std::cout << "myHex:  " << std::hex << p.myHex << "\n";
     std::cout << "myBool: " << std::dec << p.myBool << "\n";
-    std::cout << "myBuf:  " << p.myBuf << "\n";
+    std::cout << "myABuf:  " << p.myABuf << "\n";
+    std::wcout << "myWBuf:  " << p.myWBuf << "\n";
 }
 
 class DemoParams : public Params
@@ -39,8 +44,11 @@ public:
         this->addParam(new BoolParam(PARAM_MY_BOOL, false));
         this->setInfo(PARAM_MY_BOOL, "Sample boolean param");
 
-        this->addParam(new StringParam(PARAM_MY_STRING, false));
-        this->setInfo(PARAM_MY_STRING, "Sample string param");
+        this->addParam(new StringParam(PARAM_MY_ASTRING, false));
+        this->setInfo(PARAM_MY_ASTRING, "Sample string param");
+
+        this->addParam(new WStringParam(PARAM_MY_WSTRING, false));
+        this->setInfo(PARAM_MY_WSTRING, "Sample wide string param");
     }
 
     bool fillStruct(t_params_struct &paramsStruct)
@@ -54,9 +62,13 @@ public:
         IntParam *myHex = dynamic_cast<IntParam*>(this->getParam(PARAM_MY_HEX));
         if (myHex) paramsStruct.myHex = myHex->value;
 
-        StringParam *myStr = dynamic_cast<StringParam*>(this->getParam(PARAM_MY_STRING));
+        StringParam *myStr = dynamic_cast<StringParam*>(this->getParam(PARAM_MY_ASTRING));
         if (myStr) {
-            myStr->copyToCStr(paramsStruct.myBuf, sizeof(paramsStruct.myBuf));
+            myStr->copyToCStr(paramsStruct.myABuf, _countof(paramsStruct.myWBuf));
+        }
+        WStringParam *myWStr = dynamic_cast<WStringParam*>(this->getParam(PARAM_MY_WSTRING));
+        if (myWStr) {
+            myWStr->copyToCStr(paramsStruct.myWBuf, _countof(paramsStruct.myWBuf));
         }
         return true;
     }
@@ -65,21 +77,19 @@ public:
 int main(int argc, char* argv[])
 {
     DemoParams params;
-    if (argc < 2) {
-        params.info();
-        return 0;
-    }
     if (!params.parse(argc, argv)) {
-        params.info();
+        params.info(true);
         return 0;
     }
-
+    std::cout << "\nPrinting the filled params:\n";
     params.print();
 
-    std::cout << "\nConverted: \n";
     t_params_struct p;
     params.fillStruct(p);
+
+    std::cout << "\nConverted to the structure:\n";
     print_params(p);
+    std::cout << std::endl;
 
     if (!params.hasRequiredFilled()) {
         std::cout << "[!] Some of the required parameters are not filled!\n";
