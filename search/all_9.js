f5b51ad35c05be4169c78af01ffe1aa81c6f2816
hasherezade <hasherezade@users.noreply.github.com>
2021-12-28 16:00:05 +0000

    deploy: 2a86877966972d2993c5aaf80b11c057b8b2e581

--- search/all_9.js
+++ search/all_9.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['levenshtein_5fdistance_74',['levenshtein_distance',['../namespaceparamkit_1_1util.html#a2e9d23daf2a67b7265115ca5f5043321',1,'paramkit::util']]],
   ['lime_75',['LIME',['../term__colors_8h.html#a46019a1f2c10603a54b6cbb19cbf3c21',1,'term_colors.h']]],
-  ['loadboolean_76',['loadBoolean',['../namespaceparamkit.html#aac99eaa5e3c72c131dc5353a7aacc6ee',1,'paramkit']]],
+  ['loadboolean_76',['loadBoolean',['../namespaceparamkit.html#a09194681e61f03bc2e5747c6a9812326',1,'paramkit']]],
   ['loadint_77',['loadInt',['../namespaceparamkit.html#ae10aaf94114b91aa3aca8729b8ec6486',1,'paramkit']]],
   ['ltrim_78',['ltrim',['../namespaceparamkit.html#a9c4649d5e234dfbe63f7ab00cbfbe766',1,'paramkit']]]
 ];
--- search/functions_8.js
+++ search/functions_8.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['levenshtein_5fdistance_245',['levenshtein_distance',['../namespaceparamkit_1_1util.html#a2e9d23daf2a67b7265115ca5f5043321',1,'paramkit::util']]],
-  ['loadboolean_246',['loadBoolean',['../namespaceparamkit.html#aac99eaa5e3c72c131dc5353a7aacc6ee',1,'paramkit']]],
+  ['loadboolean_246',['loadBoolean',['../namespaceparamkit.html#a09194681e61f03bc2e5747c6a9812326',1,'paramkit']]],
   ['loadint_247',['loadInt',['../namespaceparamkit.html#ae10aaf94114b91aa3aca8729b8ec6486',1,'paramkit']]],
   ['ltrim_248',['ltrim',['../namespaceparamkit.html#a9c4649d5e234dfbe63f7ab00cbfbe766',1,'paramkit']]]
 ];
