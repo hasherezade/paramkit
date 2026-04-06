5f4db4c58dd136860ebef76a52acc28501ff009c
hasherezade <hasherezade@users.noreply.github.com>
2026-04-06 22:28:30 +0000

    deploy: 7206fe72130d5de2b93ef3cf1509f284892350ca

--- search/all_6.js
+++ search/all_6.js
@@ -3,7 +3,7 @@ var searchData=
   ['generalgroup_36',['generalGroup',['../classparamkit_1_1_params.html#a9915b67bf61b47d34593c0c39a8e495c',1,'paramkit::Params']]],
   ['get_5fbg_5fcolor_37',['GET_BG_COLOR',['../term__colors_8h.html#adc380188dbfb7af8f9a5899776d08745',1,'term_colors.h']]],
   ['get_5fconsole_5fcolor_38',['get_console_color',['../namespaceparamkit.html#aa9e018465904016a166fc0b6f6bb0d63',1,'paramkit']]],
-  ['get_5fnumber_39',['get_number',['../namespaceparamkit.html#a10e9e7ade6c0d1f749f21914b6d91cac',1,'paramkit']]],
+  ['get_5fnumber_39',['get_number',['../namespaceparamkit.html#a2297ac86fa3e6da8c21b5644bc9478f6',1,'paramkit']]],
   ['getintvalue_40',['getIntValue',['../classparamkit_1_1_params.html#a6917fc0a6121fb1822f5adea1fc276c9',1,'paramkit::Params']]],
   ['getname_41',['GETNAME',['../pk__util_8h.html#ae547242a5754fe70adcab0368d8288cd',1,'pk_util.h']]],
   ['getparam_42',['getParam',['../classparamkit_1_1_params.html#a5a5876b593ed0d9d18ec07cee24b57c4',1,'paramkit::Params']]],
