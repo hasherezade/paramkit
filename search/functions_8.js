c21ed0a7129bc95f400e66b0a924fa8d0e290edf
hasherezade <hasherezade@users.noreply.github.com>
2026-04-06 22:13:57 +0000

    deploy: 2de4a3f1d70e3311008e5988e1a5afffa38b5585

--- search/functions_8.js
+++ search/functions_8.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['levenshtein_5fdistance_251',['levenshtein_distance',['../namespaceparamkit_1_1util.html#a2e9d23daf2a67b7265115ca5f5043321',1,'paramkit::util']]],
   ['loadboolean_252',['loadBoolean',['../namespaceparamkit.html#a09194681e61f03bc2e5747c6a9812326',1,'paramkit']]],
-  ['loadint_253',['loadInt',['../namespaceparamkit.html#ae10aaf94114b91aa3aca8729b8ec6486',1,'paramkit']]],
+  ['loadint_253',['loadInt',['../namespaceparamkit.html#a5eed09baaeadeeb27f35935f20a02ec9',1,'paramkit']]],
   ['ltrim_254',['ltrim',['../namespaceparamkit.html#a9c4649d5e234dfbe63f7ab00cbfbe766',1,'paramkit']]]
 ];
